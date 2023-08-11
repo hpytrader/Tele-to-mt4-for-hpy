b1a37811e61e8fea3ce31a4f1a42896053df1f58
hpytrader <141956297+hpytrader@users.noreply.github.com>
2023-08-10 22:17:18 -0400

    Update run.py

--- run.py
+++ run.py
@@ -141,7 +141,7 @@ def GetTradeInformation(update: Update, trade: dict, balance: float) -> None:
     stopLossPips = abs(round((trade['StopLoss'] - trade['Entry']) / multiplier))
 
     # calculates the position size using stop loss and RISK FACTOR
-    trade['PositionSize'] = math.floor(((balance * trade['RiskFactor']*10) / stopLossPips) / 10 * 100 ) / 100 
+    trade['PositionSize'] = math.floor(((balance * trade['RiskFactor']*100) / stopLossPips) / 10 * 100 ) / 100 
     # calculates the take profit(s) in pips
     takeProfitPips = []
     for takeProfit in trade['TP']:
